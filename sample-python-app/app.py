5d865f65c827ea7291068f3c87cebb9948c163ce
Asif Hoda <126714900+Asif23feb@users.noreply.github.com>
2024-05-01 19:51:34 +0530

    Update app.py

--- sample-python-app/app.py
+++ sample-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! my name is Asif Hoda'
+    return 'Hello, world! my name is Asif Hoda.i am devops enthusiast'
 
 if __name__ == '__main__':
     app.run()
